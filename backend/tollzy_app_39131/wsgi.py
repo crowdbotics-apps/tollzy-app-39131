8c68984eeab673230f46406630fc609034a7dca4
Crowdbotics <team@crowdbotics.com>
2023-02-27 13:44:35 +0000

    Initial commit.

---
+++ backend/tollzy_app_39131/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tollzy_app_39131 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tollzy_app_39131.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tollzy-app-39131",
+  url: "https://tollzy-app-39131.botics.co",
+  api: "https://tollzy-app-39131.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
